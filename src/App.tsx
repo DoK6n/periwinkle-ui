d9d1ae42ab99f08efe56b995e4b19397e91d4824
Dok6n <ehrbs2018@gmail.com>
2023-04-24 23:55:17 +0900

    feat: apply button styles by type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import { css } from '@emotion/react'
 import { Button } from './components'
 
 function App() {
-  return <Button />
+  return (
+    <>
+      <div css={boxStyle}>
+        <Button variant='ghost'>Button</Button>
+        <Button variant='outline'>Button</Button>
+        <Button variant='link'>Button</Button>
+        {/* <Button size='lg' variant='solid'>Button</Button> */}
+        {/* <Button size='xl'>Button</Button> */}
+      </div>
+      <br />
+      <div css={boxStyle}>
+        <Button themeMode={'dark'} variant='ghost'>
+          Button
+        </Button>
+        <Button themeMode={'dark'} variant='outline'>
+          Button
+        </Button>
+        <Button themeMode={'dark'} variant='link'>
+          Button
+        </Button>
+        {/* <Button size='lg' variant='solid'>Button</Button> */}
+        {/* <Button size='xl'>Button</Button> */}
+      </div>
+      <br />
+      <div css={boxStyle}>
+        <Button size='xs'>Button</Button>
+        <Button size='sm'>Button</Button>
+        <Button>Button</Button>
+        <Button size='lg'>Button</Button>
+        <Button size='xl'>Button</Button>
+      </div>
+      <br />
+      <div css={boxStyle}>
+        <Button themeMode={'dark'} size='xs'>
+          Button
+        </Button>
+        <Button themeMode={'dark'} size='sm'>
+          Button
+        </Button>
+        <Button themeMode={'dark'}>Button</Button>
+        <Button themeMode={'dark'} size='lg'>
+          Button
+        </Button>
+        <Button themeMode={'dark'} size='xl'>
+          Button
+        </Button>
+      </div>
+    </>
+  )
 }
+
+const boxStyle = css`
+  display: flex;
+  gap: 1em;
+`
 
 export default App
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Button } from '../components'
+import { css } from '@emotion/react'
 
 const meta: Meta<typeof Button> = { title: 'Button', component: Button }
 export default meta
@@ -8,13 +9,76 @@ export default meta
 type Story = StoryObj<typeof Button>
 
 export const Default: Story = {
-  render: (args) => <Button {...args} />,
+  render: (arg) => (
+    <div
+      css={css`
+        display: flex;
+        gap: 1em;
+      `}
+    >
+      <Button {...arg} size='xs'>
+        Button
+      </Button>
+      <Button {...arg} size='sm'>
+        Button
+      </Button>
+      <Button {...arg} size='md'>
+        Button
+      </Button>
+      <Button {...arg} size='lg'>
+        Button
+      </Button>
+      <Button {...arg} size='xl'>
+        Button
+      </Button>
+    </div>
+  ),
 }
 
-export const Primary: Story = {
-  render: (args) => <Button {...args} />,
+export const PrimaryLightVariant: Story = {
+  render: ({ themeMode }) => (
+    <div
+      css={css`
+        display: flex;
+        gap: 1em;
+      `}
+    >
+      <Button themeMode={themeMode} variant='solid'>
+        Button
+      </Button>
+      <Button themeMode={themeMode} variant='ghost'>
+        Button
+      </Button>
+      <Button themeMode={themeMode} variant='outline'>
+        Button
+      </Button>
+      <Button themeMode={themeMode} variant='link'>
+        Button
+      </Button>
+    </div>
+  ),
+}
+
+export const PrimaryLightSolid: Story = {
+  render: (args) => <Button {...args}>Button</Button>,
   args: {
-    label: 'Primary',
-    bg: '700',
+    variant: 'solid',
+    themeMode: 'light',
+  },
+}
+
+export const PrimaryLightGhost: Story = {
+  render: (args) => <Button {...args}>Button</Button>,
+  args: {
+    variant: 'ghost',
+    themeMode: 'light',
+  },
+}
+
+export const PrimaryLightLink: Story = {
+  render: (args) => <Button {...args}>Button</Button>,
+  args: {
+    variant: 'link',
+    themeMode: 'light',
   },
 }
